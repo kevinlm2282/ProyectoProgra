10d06821524c8d3e0e2ca4c323d29ed88abfbd09
kevinlm2282 <55009031+kevinlm2282@users.noreply.github.com>
2022-11-17 23:13:56 -0400

    conexion a la BD

--- src/App.java
+++ src/App.java
@@ -1,11 +1,34 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
+
 import models.Docente;
 import models.Estudiante;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Estudiante estudiante = new Estudiante("juan", "campos", "12345LP", 1234567, "1111111GGGG", "secundario");
-        Docente docente = new Docente("roberto", "gimenez", "1234LLQW", 123456, "1234JKL", "doctorado");
-        System.out.println("Este es el estudiante= " + estudiante.getNombre());
-        System.out.println("Data docente= " + docente );
+        // Estudiante estudiante = new Estudiante("juan", "campos", "12345LP", 1234567, "1111111GGGG", "secundario");
+        // Docente docente = new Docente("roberto", "gimenez", "1234LLQW", 123456, "1234JKL", "doctorado");
+        // System.out.println("Este es el estudiante= " + estudiante.getNombre());
+        // System.out.println("Data docente= " + docente );
+        Conexion conexion = new Conexion();
+        Connection con = conexion.getConnection();
+
+        PreparedStatement ps;
+        ResultSet res;
+        
+        ps = con.prepareStatement("SELECT * FROM estudiante");
+        res = ps.executeQuery();
+        if(res.next()){
+            System.out.println(res.getString("apellido"));
+        }
+
+
+
+
+        
+
     }
 }
---
+++ src/Conexion.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+
+public static final String  URL = "jdbc:mysql://localhost:3306/colegio";    
+public static final String  USERNAME = "root";
+public static final String  PASSWORD = "";
+
+
+public static Connection getConnection(){
+
+    Connection connection = null;
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = (Connection) DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        System.out.println("coneccion exitosa a la base de datos");
+    } catch (Exception e) {
+        System.out.println(e);
+        // TODO: handle exception
+    }
+    return connection;
+}
+
+}
